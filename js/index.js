68ff01cbf30ad83589aa91bf2e7db8e8da871841
yasmineelmanzalawy <elmanzalawyyasmine@gmail.com>
2024-08-16 17:39:53 +0300

    finished

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const navMenu = document.getElementById("nav-menu");
 const toggle = document.getElementById("toggle");
 const toggleIcon = document.getElementById("toggleIcon");
+const navLinks = navMenu.querySelectorAll('a.nav-link');
 
 toggle.addEventListener("click", () => {
   const visible = navMenu.getAttribute("data-visible");
@@ -12,3 +13,31 @@ toggle.addEventListener("click", () => {
     toggleIcon.classList.replace('fa-x' , 'fa-border-all')
   }
 });
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.setAttribute('data-visible', 'false');
+    toggleIcon.classList.replace('fa-x', 'fa-border-all');
+  });
+});
+
+
+// animation
+
+const container =ScrollReveal({
+  distance: '120px',
+  duration: '2500',
+  delay:'100',
+  reset:true
+})
+const title =ScrollReveal({
+  distance: '60px',
+  duration: '1500',
+  reset:true
+})
+
+container.reveal(`.home-content-container`);
+container.reveal(`.home-img, .case-content, .product-content` , {origin:'top'});
+container.reveal(`.specs-img, .discount-img` , {origin:'right'});
+container.reveal(`.specs-content, .discount-content` , {origin:'left'});
+container.reveal(`.case-img, footer` , {origin:'bottom'});
+title.reveal(`.title`);
